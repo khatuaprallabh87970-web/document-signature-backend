6853e9f0754ec16b45405b9b2c40aa158e6f2370
Prallabh Prasad Khatua <khatuaprallabh87970@gmail.com>
2026-02-19 17:19:32 +0530

    fix render uploads path

--- src/routes/pdfRoutes.js
+++ src/routes/pdfRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const multer = require("multer");
 const Document = require("../models/Document");
 const authMiddleware = require("../middleware/authMiddleware");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, path.join(__dirname, "..", "..", "uploads"));
   },
   filename: (req, file, cb) => {
     const ext = file.originalname.split(".").pop();
